b90e8948e4ad698fc9c7158367292d1af33f936c
Daniel Sapkota <daniel@quantorithm.com>
2018-12-25 22:29:30 +0545

    Added run all

---
+++ runall.py
@@ -0,0 +1,28 @@
+import ipfsapi
+import psutil
+import time
+import subprocess
+from urllib.request import urlopen
+import os
+
+def ipfs_check():
+    try:
+        api = ipfsapi.connect('127.0.0.1', 5001)
+    except:
+        for proc in psutil.process_iter():
+            if proc.name() == "ipfs":
+                proc.kill()
+
+    subprocess.Popen(["ipfs","daemon"])
+
+def python_check():
+    try:
+        urlopen('https://pranish.quantorithm.com', timeout=1)
+    except:
+        subprocess.Popen(["python3","/home/warproxxx/IPFS/manage.py", "runserver"])
+        
+while True:
+    ipfs_check()
+    python_check()
+    
+    time.sleep(60)
