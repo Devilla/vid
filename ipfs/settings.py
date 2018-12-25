540f9a28b50ab5c1fcc2dd97d8f62a5357fb0557
Daniel Sapkota <daniel@quantorithm.com>
2018-12-25 19:17:10 +0545

    Added requirements

--- ipfs/settings.py
+++ ipfs/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'ipfs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ipfs_db3',
+        'NAME': 'ipfs_db2',
         'USER': 'ipfs',
         'PASSWORD': 'ipfs@pranish',
         'HOST': 'localhost',
