a975fdcaf7ce4057fe0feec1fad59c3c12945ec5
Daniel Sapkota <daniel@quantorithm.com>
2018-12-31 12:09:37 +0545

    The pricing and others

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class AssetPrice(models.Model):
+    curr_time = models.DateTimeField(auto_now_add=True)
+    steem_price = models.FloatField(default=0.269)
+    smoke_price = models.FloatField(default=0.05)
+    whaleshare_price = models.FloatField(default=0.1)
--- core/views.py
+++ core/views.py
@@ -1,16 +1,72 @@
 from django.shortcuts import render,redirect 
-from upload.models import Video, SteemVideo, WhaleShareVideo
+from upload.models import Video, SteemVideo, WhaleShareVideo, SmokeVideo
 from register.models import User
 from django.http import JsonResponse
 from django.http import HttpResponse
 from .forms import videoLikeForm, videoDisLikeForm
 import json
 import demjson
-# from blockchain_manager.manager import SteemManager, WhalesharesManger
+from core.models import AssetPrice
+from datetime import datetime
+import pytz
+import requests
+import json
+
+def get_payout(s, author, permline):
+    try:
+        acc = Comment("@{}/{}".format(author, permlink), steem_instance=s)
+        payout = float(str(acc.reward).split()[0])
+    except:
+        payout = 0.00
+    
+    return payout
 
 # Create your views here.
 def index(request):
-    
+    if (len(AssetPrice.objects.all())) == 0:
+        AssetPrice().save()
+
+    latest_price = AssetPrice.objects.all().order_by('-curr_time')[:1][0]
+    steem_price = latest_price.steem_price
+    smoke_price = latest_price.smoke_price
+    whaleshare_price = latest_price.whaleshare_price
+    latest_date = latest_price.curr_time
+    substract = datetime.utcnow().replace(tzinfo=pytz.UTC) - latest_date
+
+    if int(substract.seconds/60) >= 180:
+        try:
+            r=requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=STEEM,USD", headers={"X-CMC_PRO_API_KEY":"030f8706-dc8a-442b-82bb-8824eecf4e6e"}, timeout=1)
+            res = json.loads(r.text)
+            p = float(res['data']['STEEM']['quote']['USD']['price'])
+
+            if p > 0:
+                steem_price = p
+        except:
+            pass
+
+        try:
+            r=requests.get("https://cryptofresh.com/api/asset/markets?asset=SMOKE", timeout=0.1)
+            res = json.loads(r.text)
+            p = float(res['USD']['price'])
+
+            if p > 0:
+                smoke_price = p
+        except:
+            pass
+
+        try:
+            r=requests.get("https://cryptofresh.com/api/asset/markets?asset=WHALESHARE")
+            res = json.loads(r.text)
+            p = float(res['USD']['price'])
+
+            if p > 0:
+                whaleshare_price = p
+        except:
+            pass
+
+        a = AssetPrice(steem_price=steem_price, smoke_price=smoke_price, whaleshare_price=whaleshare_price)
+        a.save()
+
     featured = Video.objects.all().order_by('-id')[:8]
     trending = Video.objects.all().order_by('-views')[:8]
     channels = User.objects.all().order_by('-id')[:11]
@@ -21,23 +77,26 @@ def index(request):
     resolution = [2160, 1440, 1080, 720, 480, 360, 240  , 144]
 
     for each_video in featured:
-        
+
         pay = 0
-        # try:
-        #     steem = SteemVideo.objects.filter(video_id=each_video.id)
-        #     steem_pay = SteemManager.get_payout(steem.post)
-        #     pay = pay + steem_pay
-        # except: 
-        #     print('No Steem')
 
-        # try:
-        #     whale = WhaleShareVideo.objects.filter(video_id=each_video.id)
-        #     whale_pay = WhalesharesManger.get_payout(whale.post)
-        #     pay = pay + whale_pay
-        # except: 
-        #     print('No Whale')
+        try:
+            steem = SteemVideo.objects.filter(video_id=each_video.id)
+            permlink = steem[0].permlink
+            author = steem[0].author
+            #update the earning every hour too
+        except: 
+            print('No Steem')
 
-        each_video.money = pay
+        try:
+            smoke = SmokeVideo.objects.filter(video_id=each_video.id)
+        except: 
+            print('No Smoke')
+
+        try:
+            whale = WhaleShareVideo.objects.filter(video_id=each_video.id)
+        except: 
+            print('No Whaleshare')
 
         for each_res in resolution:
             if str(each_res) in each_video.video:
