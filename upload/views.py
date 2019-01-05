41a4216ac181fc32bb4985f125b83ce2adbbcfd6
Daniel Sapkota <daniel@quantorithm.com>
2019-01-05 21:20:19 +0545

    Some more fixes

--- upload/views.py
+++ upload/views.py
@@ -260,7 +260,10 @@ def post_steem(steem_key, steem_username, tags, title, body, permlink=""):
 
     s = Steem(keys=[steem_key], nodes=nodelist_one)
     if (permlink == ""):
-        s_res = s.post(title=title, body=body, author=steem_username, tags=tags, beneficiaries=[{'account': 'fiasteem', 'weight': 2500}])
+        try:
+            s_res = s.post(title=title, body=body, author=steem_username, tags=tags, beneficiaries=[{'account': 'fiasteem', 'weight': 2500}])
+        except:
+            pass
     else:
         print("Posting with permlink")
         s_res = s.post(title=title, body=body, author=steem_username, tags=tags, permlink=permlink, beneficiaries=[{'account': 'fiasteem', 'weight': 2500}])
