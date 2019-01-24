db351479e8234d50464355ca2892eac4e5e1375a
Daniel Sapkota <daniel@quantorithm.com>
2019-01-24 12:06:42 +0545

    Removing later

--- upload/views.py
+++ upload/views.py
@@ -105,13 +105,13 @@ def ajax_upload(request):
             # Generate multiple video quality to upload to the server
 
             newHash = api.add(outputName)
-            os.remove(outputName)
+            # os.remove(outputName)
 
             hash = hash + "\"" + str(720) + "\": \"" + newHash['Hash'] + "\"}"
 
             thumbnailHash = api.add(thumbnail_path)
-            os.remove(thumbnail_path)
-            
+            # os.remove(thumbnail_path)
+
             request.session['thumbnailHash'] = thumbnailHash['Hash']
             request.session['hash'] = hash
             request.session['video_only'] = newHash['Hash']
