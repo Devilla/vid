4e1abf20ba8ffa1e1aabae5d1c9735d3e17cf184
Daniel Sapkota <daniel@quantorithm.com>
2019-02-03 21:10:21 +0545

    Comments

--- advertisement/models.py
+++ advertisement/models.py
@@ -7,10 +7,10 @@ from django.contrib.postgres.fields import ArrayField
 class advertisement(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='advertisement')
     ad_title = models.CharField(max_length=255)
-    ad_banner = models.CharField(max_length=255, default="")
+    ad_banner = models.CharField(max_length=255, default="") #banner yo ma push handinchu
     active = models.BooleanField(default=False)
     currently_played = models.IntegerField(default=0)
-    total_plays = models.IntegerField(default=0)
+    total_plays = models.IntegerField(default=0) #esko value plus garne play bhayesi
     targeted_tags = ArrayField(models.CharField(max_length=200), blank=True, null=True)
     memo = models.CharField(max_length=255)
     amount = models.IntegerField()
